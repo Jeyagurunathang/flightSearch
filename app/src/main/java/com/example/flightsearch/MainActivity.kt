55f5996c1dc3d6276541ee6dd75ff087bb43b913
Jeyagurunathan G <jeyagurunathang@gmail.com>
2025-06-08 18:00:22 +0530

    Implemented the search bar

--- app/src/main/java/com/example/flightsearch/MainActivity.kt
+++ app/src/main/java/com/example/flightsearch/MainActivity.kt
@@ -4,24 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.compose.FlightSearchTheme
 import com.example.flightsearch.ui.home.HomeScreen
-import com.example.flightsearch.ui.theme.FlightSearchTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FlightSearchTheme {
+            FlightSearchTheme(dynamicColor = false) {
                 HomeScreen()
             }
         }
